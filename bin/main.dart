4177afcb30bee14695e834a8081d5b906afcf8ac
gvpaleev <gv.paleev@gmail.com>
2024-01-29 14:22:08 -0500

    v0.0.2

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,5 @@
-import 'package:dart_sdk_api_tickets/dart_sdk_api_tickets.dart' as dart_sdk_api_tickets;
+import 'package:dart_sdk_api_tickets/dart_sdk_api_tickets.dart'
+    as dart_sdk_api_tickets;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_sdk_api_tickets.calculate()}!');
